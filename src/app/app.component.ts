ae7854a4a22138da046570ac7f09634c160be011
olegkon <olegkon@yahoo.com>
2017-05-09 00:52:45 -0400

    Updated with Ag-grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { TestDataItem } from "./model";
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/switchMap';
 
+import { AgGridNg2 } from 'ag-grid-angular/main';
+import { GridOptions } from 'ag-grid/main';
+import { AgGridModule } from 'ag-grid-angular/main';
+
 
 @Component({
   selector: 'app-root',
@@ -13,14 +17,33 @@ import 'rxjs/add/operator/switchMap';
 })
 export class AppComponent {
   name:string;
-  title = 'app works!';
+  //title = 'app works!';
   searchInput: FormControl;
   dataSet: TestDataItem[];
+  gridOptions: GridOptions;
 
 
   constructor(dataService: DataService) {
     this.name = `Angular v${VERSION.full} Coding Test`;
 
+    this.gridOptions = <GridOptions>{};
+    this.gridOptions.rowData = []; // 'undefined'];
+    this.gridOptions.rowSelection = 'single';
+    // this.gridOptions.columnDefs = this.columnDefs;
+
+    this.gridOptions.columnDefs = [  // for Input grid
+      {headerName: 'Company Code', field: 'CompanyCode', width: 100},
+      {headerName: 'City', field: 'City', width: 100},
+      {headerName: 'Account', field: 'Account', width: 100},
+      {headerName: 'Country', field: 'Cuntry', width: 100},
+      {headerName: 'Credit Rating', field: 'CreditRating', width: 100},
+      {headerName: 'Currency', field: 'Currenly', width: 100},
+      {headerName: 'Amount', field: 'Amount', width: 100},
+      {headerName: 'Company Code', field: 'CompanyCode', width: 100}
+    ];
+
+    this.getData(dataService);
+/*
     this.searchInput = new FormControl('');
     this.searchInput.valueChanges
         .debounceTime(300)
@@ -29,6 +52,23 @@ export class AppComponent {
           (dataSet: TestDataItem[]) => this.dataSet = dataSet,
           error => console.error(error),
           () => console.log('Data is retrieved'));
+*/
+  }
+
+
+  getData(dataService: DataService) {  //formValue
+    dataService.getData()
+      .subscribe(
+        data => {
+          console.dir("got data: " + data);
+
+          //this.userId = formValue.userID;
+          //this.portfolios = data;
+          this.gridOptions.api.setRowData(data); 	// pass grid data and refresh display
+        },
+        err => console.log("Can't get data. Error code: %s, URL: %s ", err.status, err.url),
+        () => console.log('Done')
+      );
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule,Component, VERSION } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AgGridNg2 } from 'ag-grid-angular/main';
+import { GridOptions } from 'ag-grid/main';
+import { AgGridModule } from 'ag-grid-angular/main';
+
 import { AppComponent } from './app.component';
 import {DataService} from "./service";
 
@@ -14,7 +18,8 @@ import {DataService} from "./service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AgGridModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
